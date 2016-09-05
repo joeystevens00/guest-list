d74618ddd9c2888f411f06871f6fd382b2f532a8
joeystevens00 <joey.stevens00@gmail.com>
2016-09-05 14:00:12 -0400

    switched the order of the delete queries
    have to delete from the contact table first because the contacts table contains the foreign key fid that references the invitees table's primary key fid

--- usr/bin/guest-list-delete.sh
+++ usr/bin/guest-list-delete.sh
@@ -7,6 +7,6 @@ password=$3
 server=$4
 database=$5
 
-mysql -u $user -p$password -N -B -e "use $database; delete from invitees where fid=$fid "
 mysql -u $user -p$password -N -B -e "use $database; delete from contact where fid=$fid"
+mysql -u $user -p$password -N -B -e "use $database; delete from invitees where fid=$fid "
 
